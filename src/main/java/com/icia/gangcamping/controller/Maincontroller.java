215f9d941ceca6efd79674b893701f82abc49d09
jackthrblack <147kbj@naver.com>
2022-02-11 17:09:28 +0900

    캠핑결제페이지

--- src/main/java/com/icia/gangcamping/controller/Maincontroller.java
+++ src/main/java/com/icia/gangcamping/controller/Maincontroller.java
@@ -3,12 +3,17 @@ package com.icia.gangcamping.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class Maincontroller {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @RequestMapping("offers.html")
     public String of() {
