ff73276ad44fe2b2448b0fc6b7b55704f7624203
jackthrblack <147kbj@naver.com>
2022-02-21 22:09:02 +0900

    test

--- src/main/java/com/icia/gangcamping/dto/testDTO.java
+++ src/main/java/com/icia/gangcamping/dto/testDTO.java
@@ -1,10 +1,15 @@
 package com.icia.gangcamping.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class testDTO {
-    private String name;
+
     private String addr;
     private String tel;
+    private String name;
 }
