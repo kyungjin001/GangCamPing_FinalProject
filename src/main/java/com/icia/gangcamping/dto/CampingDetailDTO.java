6762dcfa4f69de11937f283b3a507278538f19cc
YulJung <cprpdla15@gmail.com>
2022-02-21 16:50:43 +0900

    0221

---
+++ src/main/java/com/icia/gangcamping/dto/CampingDetailDTO.java
@@ -0,0 +1,18 @@
+package com.icia.gangcamping.dto;
+
+import lombok.Data;
+
+@Data
+public class CampingDetailDTO {
+
+    private String campingName;
+
+    private String campingAddr;
+
+    private String campingInfo;
+
+    private String campingFileName;
+
+    private int campingLikeCount;
+
+}
